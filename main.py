61bf7e1222b61c1d53556b99598e0edf74ec4491
fukuyama24fi <24fi098@ms.dendai.ac.jp>
2026-06-17 23:13:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,3 +31,26 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+from fastapi.responses import HTMLResponse
+
+### コードいろいろ... ###
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.post("/present")
+async def give_present(present):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  # f文字列というPythonの機能を使っている
+
